803a9b6b094cdcc0154312a13eeee598006e2f47
Niezam <muhammadniezam29@gmail.com>
2021-10-06 12:36:46 +0800

    Update counter.blade.php

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,5 @@
-<div>
-    HI MY NAME IS NIEZAM
+<div style="text-align: center">
+    <button wire:click="increment">+</button>
+    <h2>{{$count}}</h2>
+    <button wire:click="decrement">-</button>
 </div>
