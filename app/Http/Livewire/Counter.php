9f8278f4fd857790e681cdc114c84cd6f785eb70
Niezam <muhammadniezam29@gmail.com>
2021-10-06 12:36:43 +0800

    Update Counter.php

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -6,8 +6,17 @@ use Livewire\Component;
 
 class Counter extends Component
 {
+    public $count = 1;
+    public function increment()
+    {
+        $this->count++;
+    }
+    public function decrement()
+    {
+        $this->count--;
+    }
     public function render()
     {
-        return view('livewire.counter');
+        return view('livewire.counter',['counter'=> $this->count]);
     }
 }
